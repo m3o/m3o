69e669fa677c40f1184ed478dba0c48336054ec3
Janos Dobronszki <dobronszki@gmail.com>
2021-11-08 10:26:27 +0000

    F

--- cmd/clients/ts_template.go
+++ cmd/clients/ts_template.go
@@ -68,8 +68,8 @@ const { {{ title $service.Name }}Service } = require('m3o/{{ $service.Name }}');
 {{ if endpointComment .endpoint $service.Spec.Components.Schemas }}{{ endpointComment .endpoint $service.Spec.Components.Schemas }}{{ end }}async function {{ untitle .funcName }}() {
 	let {{ $service.Name }}Service = new {{ title $service.Name }}Service(process.env.M3O_API_TOKEN)
 	let rsp = await {{ $service.Name }}Service.{{ .endpoint }}({{ tsExampleRequest $service.Name .endpoint $service.Spec.Components.Schemas .example.Request }})
-	{{ $reqType := requestType .endpoint }}{{ if isStream $service.Spec $service.Name $reqType }}console.log(rsp)
-	{{ end }}{{ if isNotStream $service.Spec $service.Name $reqType }}rsp.onMessage(msg => {
+	{{ $reqType := requestType .endpoint }}{{ if isNotStream $service.Spec $service.Name $reqType }}console.log(rsp)
+	{{ end }}{{ if isStream $service.Spec $service.Name $reqType }}rsp.onMessage(msg => {
 		console.log(msg)
 	}){{ end}}
 }
