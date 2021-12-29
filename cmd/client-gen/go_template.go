1b65966209d36ef2f5d48110d27bf229b3ec0975
Asim Aslam <asim@aslam.me>
2021-12-29 13:18:45 +0000

    generate service interface

--- cmd/client-gen/go_template.go
+++ cmd/client-gen/go_template.go
@@ -28,12 +28,9 @@ import(
 	"go.m3o.com/client"
 )
 
-func New{{ title $service.Name }}Service(token string) *{{ title $service.Name }}Service {
-	return &{{ title $service.Name }}Service{
-		client: client.NewClient(&client.Options{
-			Token: token,
-		}),
-	}
+type {{ title $service.Name }} interface {
+{{ range $key, $req := $service.Spec.Components.RequestBodies }}{{ $reqType := requestType $key }}
+{{ $endpointName := requestTypeToEndpointName $key}}{{ if endpointComment $endpointName $service.Spec.Components.Schemas }}{{ endpointComment $endpointName $service.Spec.Components.Schemas }}{{ end }}	{{ $endpointName }}(request *{{ requestType $key }}) (*{{ requestTypeToResponseType $key }}{{ if isStream $service.Spec $service.Name $reqType }}Stream{{ end }}, error)
 }
 
 type {{ title $service.Name }}Service struct {
