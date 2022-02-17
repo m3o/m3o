93bb2b5e5d505fbffefec3c920887341c26b5b7a
Martin Virando <martinvirando@hotmail.co.uk>
2022-02-17 11:31:31 +0000

    Updated the m3o function

--- snippets/m3o-request.js
+++ snippets/m3o-request.js
@@ -1,33 +1,45 @@
+/**
+ * M3O Request Snippet
+ * -------------------------
+ *
+ * Use the code below to make calls to a M3O
+ * API without the need for libraries or installs.
+ * This example uses the Fetch API.
+ */
+
 // 1. Get your m3o. This should come from an environment variable to keep secret from the public
 const key = process.env.M3O_KEY
 
 // 2. Use this code to make requests to the m3o api
 async function m3oRequest(apiName, apiMethod, data) {
-  return fetch(`https://api.m3o.com/v1/${apiName}/${apiMethod}`, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${key}`
+  const response = await fetch(
+    `https://api.m3o.com/v1/${apiName}/${apiMethod}`,
+    {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${key}`
+      }
     }
-  }).then(async (response) => {
-    const result = await response.json()
+  )
 
-    if (response.ok) {
-      return result
-    }
+  const result = await response.json()
 
-    return Promise.reject(result)
-  })
+  if (response.ok) {
+    return result
+  }
+
+  return Promise.reject(result)
 }
 
 // 3. Make a request to any of the M3O apis. In this example we will weather:
-async function getWeatherNow() {
-  const response = await m3oRequest('weather', 'Now', {
+function getWeatherNow() {
+  return m3oRequest('weather', 'Now', {
     location: 'London'
   })
-
-  console.log(response)
 }
 
-getWeatherNow()
+getWeatherNow().then((response) => {
+  console.log(response)
+})
