a32556b99e2f50ad7bc1cc162b6e25dc0df7bf84
Martin Virando <martinvirando@hotmail.co.uk>
2022-02-22 11:22:29 +0000

    Added the textarea for the creator

--- apps/emded-playground/src/scripts/app.js
+++ apps/emded-playground/src/scripts/app.js
@@ -1,3 +1,12 @@
-import m3o from 'm3o'
+import M3O from 'm3o'
+
+let m3o = M3O('MDQ3Nzk5YWQtOWM5OS00OTIzLWE4MTItMzA4MTMzMzY0OTc1')
 
 console.log(m3o)
+
+let clientCode = document.getElementById('client-code')
+let widgetRenderer = document.getElementById('widget-renderer')
+
+clientCode.oninput = (event) => {
+  widgetRenderer.innerHTML = event.target.value
+}
