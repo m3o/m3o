0ca6bd9f56e69ec4bec74bacd088c8cd02cd2d53
Martin Virando <hello@mvirando.com>
2022-02-23 19:09:10 +0000

    Added builder

--- apps/embed/public/helloworld.js
+++ apps/embed/public/helloworld.js
@@ -1,8 +1,7 @@
-let { 1: key } = document.currentScript.src.split('key=')
+let { 1: key = 'MWE1Y2RjMmUtZjdkYi00NjFhLWI1YzktYjc4Y2MxYmEyMzA2' } =
+  document.currentScript.src.split('key=')
 
-if (!key) {
-  throw new Error('No key provided')
-}
+console.log(key)
 
 document.write(
   `<iframe src="https://embed.m3o.com/hello-world.html?key=${key}" width="500" height="300"></iframe>`
