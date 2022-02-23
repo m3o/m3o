e5b0a964eebb7d23a67c13e85e3d2aa9f129b812
Martin Virando <hello@mvirando.com>
2022-02-23 12:22:19 +0000

    Removed dir

--- apps/embed-playground/public/helloworld.js
+++
@@ -1,7 +0,0 @@
-let widget = document.getElementById('m3o-helloworld-widget')
-let frame = document.createElement('iframe')
-frame.src =
-  'http://localhost:1234/widgets/hello-world.html?key=MjU4NTMyMDMtNTdlZS00ZGEyLTlhN2EtZWViZjQzMGQwYzBh'
-frame.width = '500px'
-frame.height = '250px'
-widget.appendChild(frame)
