6e3d03efbb4c0bfd5fca1f14037f392ad805042a
Martin Virando <martinvirando@hotmail.co.uk>
2022-02-24 22:10:19 +0000

    Added widget building

--- apps/embed/public/helloworld.js
+++
@@ -1,8 +0,0 @@
-let { 1: key = 'MWE1Y2RjMmUtZjdkYi00NjFhLWI1YzktYjc4Y2MxYmEyMzA2' } =
-  document.currentScript.src.split('key=')
-
-console.log(key)
-
-document.write(
-  `<iframe src="https://embed.m3o.com/hello-world.html?key=${key}" width="500" height="300"></iframe>`
-)
