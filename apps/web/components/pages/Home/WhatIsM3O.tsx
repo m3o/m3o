754d4a0771834b835f6bd29fa9393b833226abe6
Martin Virando <martinvirando@hotmail.co.uk>
2022-04-06 16:23:06 +0100

    Added changes

--- apps/web/components/pages/Home/WhatIsM3O.tsx
+++ apps/web/components/pages/Home/WhatIsM3O.tsx
@@ -6,7 +6,7 @@ import { GradientHeading } from '../../ui/GradientHeading'
 export const WhatIsM3O: FC = ({ children }) => {
   return (
     <section className="relative text-zinc-600 dark:text-zinc-400">
-      <div className="m3o-container sm p-8 md:p-16 text-center bg-zinc-100 rounded-lg dark:bg-zinc-900">
+      <div className="m3o-container sm p-8 md:p-10   text-center bg-zinc-100 rounded-lg dark:bg-zinc-900">
         {children}
       </div>
     </section>
