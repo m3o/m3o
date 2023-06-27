f8025c04f216760711b64d39d4aa12710050de70
Asim Aslam <asim@aslam.me>
2023-06-27 14:17:42 +0100

    redirect index to home when logged in (#337)
    * redirect index to home when logged in
    
    * .
    
    * .

--- chat/pages/index.tsx
+++ chat/pages/index.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Layout from '../components/layout'
 import { useGroups } from '../lib/group'
-import { acceptInvite, rejectInvite, Invite, useInvites } from '../lib/invites'
+import { useInvites } from '../lib/invites'
 import { useUser } from '../lib/user'
-import styles from './index.module.scss'
 
 export default function Home() {
     const router = useRouter()
@@ -26,79 +24,5 @@ export default function Home() {
         return <Layout loading={true} />
     }
 
-    function accept(invite: Invite) {
-        acceptInvite(invite.id)
-            .then(() => router.push(`/groups/${invite.group.id}`))
-            .catch((error: string) => {
-                alert(`Error accepting invite: ${error}`)
-            })
-    }
-
-    function reject(invite: Invite) {
-        invitesLoader.mutate(
-            invitesLoader.invites?.filter((i) => i.id !== invite.id),
-            false
-        )
-        rejectInvite(invite.id).catch((error: string) =>
-            alert(`Error rejecting invite: ${error}`)
-        )
-    }
-
-    return (
-        <Layout>
-            <div className={styles.inner}>
-                <div className={styles.titleContainer}>
-                    <h1>Welcome {userLoader.user?.first_name}</h1>
-                </div>
-
-                <div className={styles.titleContainer}>
-                    <Link href="/groups/new">
-                        <button>Create a Group</button>
-                    </Link>
-                    <Link href="/discover">
-                        <button>Join a Group</button>
-                    </Link>
-                </div>
-
-                {invitesLoader.invites?.length ? (
-                    <div>
-                        <h2 className={styles.h2}>Invites:</h2>
-                        {invitesLoader.invites?.sort((a, b) => a.group.name > b.group.name ? 1 : -1, ).map((i) => (
-                            <div key={i.id} className={styles.group}>
-                                <p>{i.group.name}</p>
-                                <button
-                                    onClick={() => accept(i)}
-                                    className={styles.accept}
-                                >
-                                    Accept
-                                </button>
-                                <button
-                                    onClick={() => reject(i)}
-                                    className={styles.reject}
-                                >
-                                    Reject
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                ) : null}
-
-                {groupsLoader.groups?.length ? (
-                    <div>
-                        <h2 className={styles.h2}>Your Groups</h2>
-                        <div className={styles.groups}>
-                            {groupsLoader.groups?.sort((a, b) => a.name > b.name ? 1 : -1, ).map((g) => (
-                                <div key={g.id} className={styles.group}>
-                                    <p>{g.name}</p>
-                                    <Link href={`/groups/${g.id}`}>
-                                        <button>Enter</button>
-                                    </Link>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                ) : null}
-            </div>
-        </Layout>
-    )
+    router.push("/home")
 }
