34b24ed3b871d766eecf1f2ed11dae590efbf487
asim <asim@aslam.me>
2023-07-03 17:34:10 +0100

    link updates

--- web/components/ui/Footer/Footer.tsx
+++ web/components/ui/Footer/Footer.tsx
@@ -11,8 +11,8 @@ interface ExternalLink {
 
 const EXTERNAL_LINKS_1: ExternalLink[] = [
   {
-    name: 'Chat',
-    link: 'https://m3o.chat',
+    name: 'Blog',
+    link: 'https://blog.m3o.com',
   },
   {
     name: 'Email',
