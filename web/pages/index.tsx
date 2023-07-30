608c30eed3dbb6d2a597365f0ab064085fdea172
asim <asim@aslam.me>
2023-07-30 21:49:00 +0100

    replace index

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,46 +1,34 @@
-import type { ReactElement } from 'react'
+import type { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 import seo from '@/lib/seo.json'
-import { LoggedInView } from '@/components/pages/Home'
-import { WithAuthProps, withAuth } from '@/lib/api/m3o/withAuth'
-import { AuthCookieNames, Routes } from '@/lib/constants'
+import { MainLayout } from '@/components/layouts'
+import { WithAuthProps } from '@/lib/api/m3o/withAuth'
+import {
+  Explore,
+  ExploreHeader,
+  ExploreProps,
+  exploreGetServerSideProps,
+} from '@/components/ui'
 
-interface Props extends WithAuthProps {
-  apiToken: string
-}
+export const getServerSideProps = exploreGetServerSideProps
 
-export const getServerSideProps = withAuth(async context => {
-  if (!context.req.user) {
-    return {
-      redirect: {
-        destination: Routes.Login,
-        permanent: false,
-      },
-    }
-  }
-
-  return {
-    props: {
-      apiToken: context.req.cookies[AuthCookieNames.ApiToken] || '',
-      user: context.req.user,
-    } as Props,
-  }
-})
-
-export default function Home({
-  apiToken,
+const Home: NextPage<ExploreProps & WithAuthProps> = ({
   user,
-}: Props): ReactElement {
+  ...exploreProps
+}) => {
   return (
     <>
       <NextSeo
         title={user ? seo.home.title : seo.landing.title}
         description={user ? seo.home.description : seo.landing.description}
+        title={seo.explore.title}
         canonical="https://m3o.com"
       />
-      {user ? (
-        <LoggedInView user={user} apiToken={apiToken} />
-      ) : (<div></div>) }
+      <MainLayout>
+        <Explore {...exploreProps} header={<ExploreHeader title="M3O Services" />} />
+      </MainLayout>
     </>
   )
 }
+
+export default Home
